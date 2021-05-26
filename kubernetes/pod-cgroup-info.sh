6d8e782f2eb0dea62450236973491c211ceaf1c7
Jianshao <787903+jianshaow@users.noreply.github.com>
2021-05-26 14:16:46 +0800

    update

--- kubernetes/pod-cgroup-info.sh
+++ kubernetes/pod-cgroup-info.sh
@@ -1,18 +1,42 @@
 #!/bin/sh
+cgroup_subsystems='cpu memory'
+
 
 print_cgroup_info() {
   remote_node=$1
   cgroup_path=$2
-  cpu_shares=$(ssh $remote_node cat $cgroup_path/cpu.shares)
-  cfs_period_us=$(ssh $remote_node cat $cgroup_path/cpu.cfs_period_us)
-  cfs_quota_us=$(ssh $remote_node cat $cgroup_path/cpu.cfs_quota_us)
-  cpu_stat=$(ssh $remote_node cat $cgroup_path/cpu.stat)
+
+  for subsystem in $cgroup_subsystems; do
+    cgroup_cpu_path=${cgroup_path//@subsystem@/$subsystem}
+    print_cgroup_${subsystem}_info $remote_node $cgroup_cpu_path
+    echo
+  done
+}
+
+print_cgroup_cpu_info() {
+  remote_node=$1
+  cgroup_cpu_path=$2
+
+  cpu_shares=$(ssh $remote_node cat $cgroup_cpu_path/cpu.shares)
+  cfs_period_us=$(ssh $remote_node cat $cgroup_cpu_path/cpu.cfs_period_us)
+  cfs_quota_us=$(ssh $remote_node cat $cgroup_cpu_path/cpu.cfs_quota_us)
+  cpu_stat=$(ssh $remote_node cat $cgroup_cpu_path/cpu.stat)
 
   echo [cpu]
   echo "cpu_share:       $cpu_shares"
   echo "cfs_period_us:   $cfs_period_us"
   echo "cfs_quota_us:    $cfs_quota_us"
   echo "cpu_stat:        "$cpu_stat
+}
+
+print_cgroup_memory_info() {
+  remote_node=$1
+  cgroup_memory_path=$2
+
+  limit_in_bytes=$(ssh $remote_node cat $cgroup_memory_path/memory.limit_in_bytes)
+
+  echo [memory]
+  echo "limit_in_bytes:  $limit_in_bytes"
 }
 
 get_container_cgroup_info() {
@@ -26,7 +50,10 @@ get_container_cgroup_info() {
   container_uid=${container_id#*//}
 
   container_cgroup_path=${parent_path}/docker-$container_uid.scope
-  echo -------------------------- $container_name --------------------------
+  echo ------------------------------------------------------------------------------------------
+  echo "container_name: $container_name"
+  echo "container_id:   $container_id"
+  echo ------------------------------------------------------------------------------------------
   # echo get container $container_name cgroup info on $container_cgroup_path
 
   print_cgroup_info $node $container_cgroup_path
@@ -52,8 +79,14 @@ get_pod_cgroup_info() {
 
   pod_dir=${pod_uid//\-/\_}
 
-  pod_cgroup_path=/sys/fs/cgroup/cpu/kubepods.slice${cgroup_path_prefix}.slice${cgroup_path_prefix}-pod${pod_dir}.slice
-  echo ========================== $pod_name ==========================
+  pod_cgroup_path=/sys/fs/cgroup/@subsystem@/kubepods.slice${cgroup_path_prefix}.slice${cgroup_path_prefix}-pod${pod_dir}.slice
+  echo ==========================================================================================
+  echo "pod_name:     $pod_name"
+  echo "pod_uid:      $pod_uid"
+  echo "node_name:    $node_name"
+  echo "host_ip:      $host_ip"
+  echo "qos_class:    $qos_class"
+  echo ==========================================================================================
   # echo get cgroup info from pod $pod_name on $node_name:$pod_cgroup_path
 
   print_cgroup_info $node_name $pod_cgroup_path
