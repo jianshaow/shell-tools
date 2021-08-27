fa5be1767f756dd5263cf99835e2a7d7634b584f
Jianshao <787903+jianshaow@users.noreply.github.com>
2021-08-27 11:58:19 +0800

    improve

--- kubernetes/pod-resource-to-csv.sh
+++ kubernetes/pod-resource-to-csv.sh
@@ -8,7 +8,7 @@ fi
 . env.sh
 . pod-common.sh
 
-jp_resources='{.name}{","}{.resources.requests.cpu}{","}{.resources.requests.memory}{","}{.resources.limits.cpu}{","}{.resources.limits.memory}{"\n"}'
+jp_resources='{.name}{"|"}{.resources.requests.cpu}{"|"}{.resources.requests.memory}{"|"}{.resources.limits.cpu}{"|"}{.resources.limits.memory}{"\n"}'
 
 cpu_func='function cpu(cpus) { i=index(cpus,"m"); if(i==length(cpus)) { return substr(cpus,1,i-1) } else { return cpus*1000 }}'
 memory_func='function memory(mems) { i=index(mems,"Mi"); l=length(mems); if(i==l-1) { return substr(mems,1,i-1) } else { return strtonum(substr(mems,1,l-2))*1024 }}'
@@ -17,13 +17,9 @@ print_pod_resources_by_label() {
   pod_label=$1
   prefix=$2
 
-  if [[ "$prefix" =~ "," ]]; then
-    print_code='{ print $1","$2","$3","cpu($4)","memory($5)","cpu($6)","memory($7) }'
-  else
-    print_code='{ print $1","$2","cpu($3)","memory($4)","cpu($5)","memory($6) }'
-  fi
+  print_code='{ if(last==$1) { printf "," } else {last=$1; printf $1","} print $2","cpu($3)","memory($4)","cpu($5)","memory($6) }'
 
-  kubectl -n $ns get po -l $pod_label -ojsonpath='{range .items[0].spec.containers[*]}{"'$prefix',"}'$jp_resources'{end}' | awk -F ',' "$cpu_func $memory_func $print_code"
+  kubectl -n $ns get po -l $pod_label -ojsonpath='{range .items[0].spec.containers[*]}{"'$prefix'|"}'$jp_resources'{end}' | awk -F "|" "$cpu_func $memory_func $print_code"
 }
 
 print_pod_resources_by_workload() {
@@ -38,16 +34,12 @@ print_pod_resources_by_workload() {
     owner=$workload_name
   fi
 
-  if [[ "$prefix" =~ "," ]]; then
-    print_code='{ print $1","$2","$3","cpu($4)","memory($5)","cpu($6)","memory($7) }'
-  else
-    print_code='{ print $1","$2","cpu($3)","memory($4)","cpu($5)","memory($6) }'
-  fi
+  print_code='{ if(last==$1) { printf "," } else {last=$1; printf $1","} print $2","cpu($3)","memory($4)","cpu($5)","memory($6) }'
 
   pod_names=$(kubectl -n $ns get pod -l $pod_label -ojsonpath='{range .items[?(@.metadata.ownerReferences[0].name=="'$owner'")]}{.metadata.name}{"\n"}{end}')
   array=($pod_names)
 
-  kubectl -n $ns get po ${array[0]} -ojsonpath='{range .spec.containers[*]}{"'$prefix',"}'$jp_resources'{end}' | awk -F ',' "$cpu_func $memory_func $print_code"
+  kubectl -n $ns get po ${array[0]} -ojsonpath='{range .spec.containers[*]}{"'$prefix'|"}'$jp_resources'{end}' | awk -F "|" "$cpu_func $memory_func $print_code"
 }
 
 print_pod_resources_of_workload() {
