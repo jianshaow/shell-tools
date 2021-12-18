02277e23ef8ddcec3b41e2e1823107b32c616b24
Jianshao <787903+jianshaow@users.noreply.github.com>
2021-12-18 19:56:33 +0800

    add arthus

---
+++ kubernetes/arthus.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ ! -f "env.sh" ]; then
+  echo an env.sh needed, refer to example
+  exit 1
+fi
+
+. env.sh
+. pod-common.sh
+
+profiler() {
+  pod_name=$1
+  container_name=$2
+  pod_exec $pod_name $container_name 'cd /tmp; curl -O https://arthas.aliyun.com/arthas-boot.jar'
+  process_id = $(pod_exec $pod_name $container_name "jps|grep -v Jps|awk '{print $1}'")
+  pod_exec $pod_name $container_name "java -jar /tmp/arthas-boot.jar $process_id 'profiler list'"
+}
+
+case $1 in
+  profiler)
+    profiler $2 $3
+    ;;
+  *)
+    usage
+    ;;
+esac
